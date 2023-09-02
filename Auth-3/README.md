83fbe237802e56a9d73cbee91564ccbaa9261b21
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-09-02 12:19:26 +0530

    auth using local storage and mern

---
+++ Auth-3/README.md
@@ -0,0 +1,25 @@
+# Authentication (JWT)
+
+## Tech Stacks Used
+- ReactJS (npx)
+- Local Storage
+- Google Signin
+- NodeJS
+- ExpressJS
+- MongoDB
+
+## Routes Used
+Frontend :
+Public routes
+- ```/```                    : Landing page 
+- ```/login```               : Login page 
+- ```/register```            : Register page 
+Private routes
+- ```/addTour```             : Add Tour Page 
+- ```/editTour/:id```        : Edit Tour Page 
+- ```/dashboard```            : Dashboard 
+
+Backend :
+- ```/users/googleSignIn```               : 
+- ```/users/signup```          : login route
+- ```/users/signin```      : register route
---
+++ Auth-3/server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
+import userRouter from "./routes/user.js";
+import tourRouter from "./routes/tour.js";
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/users", userRouter); // http://localhost:5000/users/signup
+app.use("/tour", tourRouter);
+
+const MONGODB_URL = "Your MongoDB URL";
+const port = process.env.PORT || 5000;
+
+mongoose
+  .connect("mongodb+srv://sayan:sayan@cluster0.hhmv3t2.mongodb.net/")
+  .then(() => {
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
